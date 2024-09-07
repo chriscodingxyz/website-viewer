44533c70e1a38de18c96b611bfb0dccf970207fd
cherrydub <cherrydub@gmail.com>
2024-09-08 00:31:20 +0100

    header text added

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,9 +10,17 @@ export function Header() {
   return (
     <header className="border-b px-4">
       <div className="container mx-auto flex justify-between items-center py-4">
-        <h1 className={`text-5xl font-bold ${monofett.className}`}>
-          Website Viewer
-        </h1>
+        <div>
+          <h1 className={`text-5xl font-bold ${monofett.className}`}>
+            Website Viewer
+          </h1>
+          <h1
+            className={`text-2xl font-bold text-blue-700 ${monofett.className}`}
+          >
+            {" "}
+            Layout Lab
+          </h1>
+        </div>
         <ThemeToggle />
       </div>
     </header>
