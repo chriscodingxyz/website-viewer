b9b674538e5711a56b1ae3634117679ce4c76c90
cherrydub <cherrydub@gmail.com>
2025-11-11 11:48:45 +0000

    fix: other tabs attempt 5

--- components/sections/AnalysisSection.tsx
+++ components/sections/AnalysisSection.tsx
@@ -19,6 +19,7 @@ interface AnalysisSectionProps {
 
 export default function AnalysisSection({ expanded, onToggle }: AnalysisSectionProps) {
   const { metadata, metadataLoading, metadataError, fetchMetadata, currentSite } = useWebsiteViewer()
+  console.log('🔥 [AnalysisSection] State:', { hasMetadata: !!metadata, metadataLoading, metadataError })
 
   // Helper functions from OverviewDashboard
   const getSEOScore = (metadata: WebsiteMetadata | null) => {
